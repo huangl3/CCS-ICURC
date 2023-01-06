77709582366b373652199d7e23fd555aa54d4e49
Longxiu Huang <55358808+huangl3@users.noreply.github.com>
2023-01-06 15:16:01 -0500

    Add files via upload

---
+++ ICURC/SetDefaultParams_CCS.m
@@ -0,0 +1,15 @@
+function s = SetDefaultParams_CCS(s)
+% s = SetDefaultsms(s);
+% Sets default smeters
+% s: user-specified set of parameters that are used instead of defaults
+
+if (isfield(s, 'p') == 0),
+    s.p = 0.2;
+end
+
+if (isfield(s, 'delta') == 0),
+    s.delta = 0.2;
+end
+
+end
+

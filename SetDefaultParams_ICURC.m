37b6bd4b03cf81ea5faffb850703f17066b5fb62
HanQin Cai <44795174+caesarcai@users.noreply.github.com>
2023-07-03 22:10:32 -0400

    Update SetDefaultParams_ICURC.m

--- SetDefaultParams_ICURC.m
+++ SetDefaultParams_ICURC.m
@@ -3,20 +3,21 @@ function s = SetDefaultParams_ICURC(s)
 % Sets default smeters
 % s: user-specified set of parameters that are used instead of defaults
 
-if (isfield(s, 'TOL') == 0),
+if (isfield(s, 'TOL') == 0)
     s.TOL = 1e-4;
 end
 
-if (isfield(s, 'max_ite') == 0),
+if (isfield(s, 'max_ite') == 0)
     s.max_ite = 500;
 end
 
-if (isfield(s, 'eta') == 0),
+if (isfield(s, 'eta') == 0)
     s.eta = [1, 1, 1];
+    s.steps_are1 = true;
+elseif (s.eta == [1, 1, 1])
     s.steps_are1 = true;
 else
     s.steps_are1 = false;
 end
 
 end
-
